dd0b303d6c00e849fe91c0822d914cc092160643
杨俊宁 <youngjuning@aliyun.com>
2020-08-28 14:38:17 +0800

    Update repos.js

--- repos.js
+++ repos.js
@@ -1,26 +1,7 @@
 module.exports = [
-  {
-    owner: "didi",
-    repo: "cube-ui",
-  },
-  {
-    owner: "YMFE",
-    repo: "yapi",
-  },
-  {
-    owner: "didi",
-    repo: "cube-ui",
-  },
-  {
-    owner: "XiaoMi",
-    repo: "soar",
-  },
-  {
-    owner: "remaxjs",
-    repo: "remax",
-  },
-  {
-    owner: "typicode",
-    repo: "husky",
-  }
+  { owner: "YMFE", repo: "yapi" },
+  { owner: "didi", repo: "cube-ui" },
+  { owner: "XiaoMi", repo: "soar" },
+  { owner: "remaxjs", repo: "remax" },
+  { owner: "typicode", repo: "husky" }
 ]
