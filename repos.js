b6a2ae3fc0361fe2378615626d2becab01d6961b
杨俊宁 <youngjuning@aliyun.com>
2020-08-31 18:26:03 +0800

    Add: juejin-im/open-source

--- repos.js
+++ repos.js
@@ -3,5 +3,6 @@ module.exports = [
   { owner: "didi", repo: "cube-ui" },
   { owner: "XiaoMi", repo: "soar" },
   { owner: "remaxjs", repo: "remax" },
-  { owner: "typicode", repo: "husky" }
+  { owner: "typicode", repo: "husky" },
+  { owner: "juejin-im", repo: "open-source" },
 ]
